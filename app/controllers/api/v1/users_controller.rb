1c567d92d9ed50a97b72fdc2676f138175bd4038
JakesProjects1 <jacob@kagon.net>
2021-01-31 21:36:53 -0500

    test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
   
-skip_before_action :authorized, only: [:user_reviews, :create, :update]
+skip_before_action :authorized, only: [:user_reviews, :create, :update, :index]
 
     def index
         @users = User.all
