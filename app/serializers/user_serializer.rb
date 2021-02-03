c4737b41c21324f85354e7d371bae5e520767607
JakesProjects1 <jacob@kagon.net>
2021-02-03 12:40:33 -0500

    lets try this again

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,16 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :user_id, :first_name, :last_name, :username, :followers(exclude: [:password_digest]), :followees, :reviews, :watchlists, :followers_count, :followee_count
+  attributes :user_id, :first_name, :last_name, :username, :_select_followers, :select_followees, :reviews, :watchlists, :followers_count, :followee_count
  
-
-
   def followers_count
     object.followers.count
+  end
+
+  def select_followers
+    object.followers.select(:id, :first_name, :last_name, :username)
+  end
+
+  def select_followees
+    object.followees.select(:id, :first_name, :last_name, :username)
   end
 
   
