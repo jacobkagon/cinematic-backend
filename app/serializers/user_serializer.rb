2e4c059d6860c4722a7100052f4673d75e694a11
JakesProjects1 <jacob@kagon.net>
2021-02-03 12:42:22 -0500

    again

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :user_id, :first_name, :last_name, :username, :_select_followers, :select_followees, :reviews, :watchlists, :followers_count, :followee_count
+  attributes :user_id, :first_name, :last_name, :username, :select_followers, :select_followees, :reviews, :watchlists, :followers_count, :followee_count
  
   def followers_count
     object.followers.count
