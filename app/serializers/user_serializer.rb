c5a932819469b9b84059e67ee1b583674571f9c2
JakesProjects1 <jacob@kagon.net>
2021-02-03 14:14:36 -0500

    will this work

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :reviews, :watchlists, :followers_count, :followee_count, :followers.only(:first_name, :last_name, :username, :id), :followees
+  attributes :id, :first_name, :last_name, :username, :reviews, :watchlists, :followers_count, :followee_count, :followers.select(:first_name, :last_name, :username, :id), :followees
   
   
   def followers_count
