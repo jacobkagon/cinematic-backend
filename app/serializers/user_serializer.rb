6a2f599040c34871842fa982ac040c571d107f9e
JakesProjects1 <jacob@kagon.net>
2021-02-03 12:00:31 -0500

    let's see if these serializers work

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,8 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :reviews, :watchlists, :followers_count, :followee_count, :followers, :followees
-  
+  attributes :id, :first_name, :last_name, :username, :reviews, :followers_count, :followee_count
+  has_many :followers, exclude: :email, :password_digest
+  has_many :followees, exclude: :email, :password_digest
+  has_many :watchlists, exclude: :created_at, :updated_at
 
   def followers_count
     object.followers.count
