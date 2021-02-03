0b6b8f6bc511b28fd21fc3260532077608ac4972
JakesProjects1 <jacob@kagon.net>
2021-02-03 14:03:12 -0500

    update followers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :reviews, :watchlists, :followers_count, :followee_count, :followers, :followees
+  attributes :id, :first_name, :last_name, :username, :reviews, :watchlists, :followers_count, :followee_count
 
+  has_many :followers
+  has_many :followees
+  
+  
   def followers_count
     object.followers.count
   end
