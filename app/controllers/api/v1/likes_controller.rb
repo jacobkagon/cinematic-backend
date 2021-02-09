6a4e59cd874bd5796acd461404f3a6046c187e33
JakesProjects1 <jacob@kagon.net>
2021-02-09 11:16:54 -0500

    added changes to likes table

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::LikesController < ApplicationController
+    skip_before_action :authorized, only: [:create]
 
     def create
         @like = Like.create(user_id: params[:user_id], review_id: params[:review_id])
