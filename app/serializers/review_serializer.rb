ef9b68a85a1ca946119a92b97e778cb994bb44e3
JakesProjects1 <jacob@kagon.net>
2021-02-09 11:37:13 -0500

    added likes to review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :body, :rating, :movie, :created_at, :user
+  attributes :id, :body, :rating, :movie, :created_at, :user, :likes
 end
